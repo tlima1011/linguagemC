6e3afbc0d96d77409344b4406a33441e28a0549a
Thiago <thiagolima1011@gmail.com>
2020-08-19 12:51:12 -0300

    Atualizacao

--- Ex4/main.c
+++ Ex4/main.c
@@ -3,15 +3,22 @@
 
 int main()
 {
-    double media, nota1, nota2;
+    double media, nota1, nota2, nota3, nota4;
 
     printf("Informe a primeira nota: ");
     scanf("%lf", &nota1);
 
     printf("Informe a segunda nota: ");
     scanf("%lf", &nota2);
+    
+    printf("Informe a terceira nota: ");
+    scanf("%lf", &nota3);
+    
+    printf("Informe a quarta nota: ");
+    scanf("%lf", &nota4);
+    
 
-    media = (nota1 + nota2) / 2.0;
+    media = (nota1 + nota2 + nota3 + nota4) / 4.0;
     printf("A media = %.2lf", media);
 
     //gletch();
