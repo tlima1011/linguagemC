fb0cf3041bfaa0785f9cf97fb22292e0858d3906
Thiago <thiagolima1011@gmail.com>
2020-08-19 12:41:42 -0300

    Exercicio2

---
+++ Ex2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numero;
+
+    printf("Informe um numero: ");
+    scanf("%d", &numero);
+
+    printf("O numero digitado foi: %d", numero);
+
+    //gletch();
+    return 0;
+}
