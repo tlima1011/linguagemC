459dd3ea7e1a25d484298b6f16c555c618a9f7b4
Thiago <thiagolima1011@gmail.com>
2020-08-20 13:27:30 -0300

    Exercício 6

---
+++ aula17/ex06/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+int main()
+{
+
+    for(int i = 1; i <= 20;i++){
+        printf("%d\n",i);
+    }
+
+    for(int i = 1; i <= 20;i++){
+        printf("%d ",i);
+    }
+
+    return 0;
+}
