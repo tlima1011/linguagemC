57e544da2153bca76f8bea7db43ee1e80e3dc628
Thiago <thiagolima1011@gmail.com>
2020-08-20 14:05:45 -0300

    Exercício 10

---
+++ aula17/ex10/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+int main()
+{
+
+    int troca = 0, num1 = 0, num2 = 0;
+    printf("Informe o primeiro numero: ");
+    scanf("%d", &num1);
+    printf("Informe o segundo numero: ");
+    scanf("%d", &num2);
+
+    if(num1 > num2){
+        troca = num1;
+        num1 = num2;
+        num2 = troca;
+    }
+
+    for(int i = num1; i < num2;i++){
+        //i++;
+        if(i == num1){
+            i++;
+            printf("%d ", i);
+        }else{
+            printf("%d ", i);
+        }
+    }
+    getch();
+    return 0;
+}
