3f88e14149593c7e51ab004086c6b8cdad94da15
Thiago <thiagolima1011@gmail.com>
2020-08-20 14:36:43 -0300

    Exercício 16

---
+++ aula17/ex16/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+int main()
+{
+
+    int n = 0, n1 = 0, n2 = 1, n3 = 0;
+
+    printf("Quantidade de N: ");
+    scanf("%d", &n);
+    printf("%d %d ", n1, n2);
+    for(int i = 0; i < 500 - 2;i++){
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+        printf("%d ",n3);
+    }
+    printf("\n");
+    getch();
+    return 0;
+}
