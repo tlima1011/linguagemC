0561c076c69aeeb461203ebf95de7b5bb0362266
Thiago <thiagolima1011@gmail.com>
2020-08-20 16:04:10 -0300

    Exercício 20

---
+++ aula17/ex20/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+int main()
+{
+    double media = 0, soma = 0;
+    int n = 0, idade = 0;
+
+    printf("Informe o numero de idades: ");
+    scanf("%d", &n);
+
+    for(int i = 0; i < n;i++){
+        printf("Informe a %d. idade: ", (i + 1));
+        scanf("%d",&idade);
+        soma += idade;
+    }
+    media = soma / n;
+    printf("Media de Idades = %.1lf",media);
+    printf("\n");
+    getch();
+    return 0;
+}
